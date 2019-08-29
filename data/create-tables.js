dd1779fc6117fa68309106fceceaaf312b89c59d
JOHN NELSON <JohnNelson4850@gmail.com>
2019-08-29 11:27:38 -0700

    intitialized auth-extensions and set up server.js

--- data/create-tables.js
+++ data/create-tables.js
@@ -3,6 +3,13 @@ const client = require('../lib/client');
 client.connect()
     .then(() => {
         return client.query(`
+            CREATE TABLE users (
+                id SERIAL PRIMARY KEY,
+                email VARCHAR(256) NOT NULL,
+                hash VARCHAR(512) NOT NULL,
+                display_name VARCHAR(256) NOT NULL
+            );
+
             CREATE TABLE tasks (
                 id SERIAL PRIMARY KEY NOT NULL,
                 name VARCHAR(256) NOT NULL,
@@ -12,7 +19,7 @@ client.connect()
         `);
     })
     .then(
-        () => console.log('tasks created'),
+        () => console.log('tables created'),
         err => console.log(err)
     )
     .then(() => {
